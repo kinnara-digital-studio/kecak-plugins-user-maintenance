d0b4c683b20189e7c982e6a779542237771c606e
aristosh <aristo.hadisoeganda@kinnarastudio.com>
2021-12-29 20:37:02 +0700

    fix empty dir_user_salt

--- src/main/java/com/kinnara/kecakplugins/usermaintenance/utils/PasswordUtilMixin.java
+++ src/main/java/com/kinnara/kecakplugins/usermaintenance/utils/PasswordUtilMixin.java
@@ -1,5 +1,6 @@
 package com.kinnara.kecakplugins.usermaintenance.utils;
 
+import com.kinnarastudio.commons.Try;
 import org.joget.apps.app.service.AppUtil;
 import org.joget.commons.util.HashSalt;
 import org.joget.commons.util.PasswordGeneratorUtil;
@@ -9,18 +10,27 @@ import org.joget.directory.model.User;
 import org.joget.directory.model.UserSalt;
 import org.joget.directory.model.service.DirectoryUtil;
 import org.joget.directory.model.service.UserSecurity;
+import org.joget.workflow.util.WorkflowUtil;
 import org.springframework.context.ApplicationContext;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Date;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface PasswordUtilMixin {
-    default void updatePassword(User user) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        UserSecurity us = DirectoryUtil.getUserSecurity();
-        ApplicationContext applicationContext = AppUtil.getApplicationContext();
-        UserDao userDao = (UserDao) applicationContext.getBean("userDao");
-        UserSaltDao userSaltDao = (UserSaltDao) applicationContext.getBean("userSaltDao");
-        UserSalt userSalt = userSaltDao.getUserSaltByUserId(user.getUsername());
+    default void updatePassword(@Nonnull User user) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        final UserSecurity us = DirectoryUtil.getUserSecurity();
+        final ApplicationContext applicationContext = AppUtil.getApplicationContext();
+        final UserDao userDao = (UserDao) applicationContext.getBean("userDao");
+        final UserSaltDao userSaltDao = (UserSaltDao) applicationContext.getBean("userSaltDao");
+        final UserSalt userSalt = Optional.of(user)
+                .map(User::getUsername)
+                .map(userSaltDao::getUserSaltByUserId)
+                .orElseGet(Try.onSupplier(() -> generateSalt(user)));
 
         if (user.getPassword() != null && user.getConfirmPassword() != null && user.getPassword().length() > 0 && user.getPassword().equals(user.getConfirmPassword())) {
             if (us != null) {
@@ -40,5 +50,28 @@ public interface PasswordUtilMixin {
         if (us != null) {
             us.updateUserProfilePostProcessing(user);
         }
+    }
+
+    @Nonnull
+    default UserSalt generateSalt(User user) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        final ApplicationContext applicationContext = AppUtil.getApplicationContext();
+        final UserSaltDao userSaltDao = (UserSaltDao) applicationContext.getBean("userSaltDao");
+
+        final String currentUsername = WorkflowUtil.getCurrentUsername();
+        final Date now = new Date();
+        final UserSalt newUserSalt = new UserSalt();
+
+        newUserSalt.setId(UUID.randomUUID().toString());
+        newUserSalt.setUserId(user.getId());
+        newUserSalt.setCreatedBy(currentUsername);
+        newUserSalt.setModifiedBy(currentUsername);
+        newUserSalt.setDateCreated(now);
+        newUserSalt.setDateModified(now);
+
+        final HashSalt hashSalt = PasswordGeneratorUtil.createNewHashWithSalt(user.getPassword());
+        newUserSalt.setRandomSalt(hashSalt.getSalt());
+        userSaltDao.addUserSalt(newUserSalt);
+
+        return newUserSalt;
     }
 }
