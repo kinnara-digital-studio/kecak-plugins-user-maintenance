09473db033732a3df4ab8dc75430a8561b29e6e6
Nathanael-Adi <6181901041@student.unpar.ac.id>
2025-05-27 10:08:45 +0700

    rename user directory form binder

--- src/main/java/com/kinnarastudio/kecakplugins/usermaintenance/form/binder/UserDirectoryFormBinder.java
+++ src/main/java/com/kinnarastudio/kecakplugins/usermaintenance/form/binder/UserDirectoryFormBinder.java
@@ -185,7 +185,7 @@ public class UserDirectoryFormBinder extends DefaultFormBinder implements FormLo
 
     @Override
     public String getLabel() {
-        return "User Directory Form Binder";
+        return "User Maintenance Directory Form Binder";
     }
 
     @Override
